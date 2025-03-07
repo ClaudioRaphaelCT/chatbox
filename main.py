85a8ad20cf69fc7e6917288227f0eea1f8e1af9a
Claudio Raphael Cabral Teixeira <crteixeira@topaz.corp>
2025-03-07 08:50:07 -0300

    testando

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app  =  FastAPI()
+
+@app.post("/wpp")
+async def whatsapp_reply(request: Request):
+    form_data = await request.form()
+    body = form_data.get("Body", "").strip().lower()
+    
+    response = MessagingResponse()
+    if body == "ola":
+        response.message("Bem-vindo!")
+    else:
+        response.message("Desculpe, não entendi.")
+    
+    return str(response)
+        
